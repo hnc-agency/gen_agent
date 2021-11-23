91449a75f56697d0ff91fdcd43bd7c17ed0dac7f
Maria Scott <maria-12648430@hnc-agency.org>
2021-11-23 17:08:59 +0100

    Typespecs

--- src/gen_agent.erl
+++ src/gen_agent.erl
@@ -38,6 +38,11 @@
 
 -export([callback_mode/0, init/1, handle_event/4, terminate/3, code_change/4]).
 
+-type server_ref() :: gen_statem:server_ref().
+-type start_opt() :: gen_statem:start_opt().
+-type start_ret() :: {ok, pid()} | ignore | {error, term()}.
+-type start_mon_ret() :: {ok, {pid(), reference()}} | ignore | {error, term()}.
+-type from() :: gen_statem:from().
 -type agent_state() :: idle | sleeping | executing.
 
 -callback init(Args) -> Result
@@ -80,7 +85,7 @@
 
 -callback handle_event(EventType, Message, Data0) -> Result
 	when EventType :: {call, From} | cast | info | timeout,
-	     From :: gen_statem:from(),
+	     From :: from(),
 	     Message :: term(),
 	     Data0 :: term(),
 	     Result :: done
@@ -101,7 +106,7 @@
 
 -callback handle_command(EventType, Message, State, Data0) -> Result
 	when EventType :: {call, From} | cast | timeout,
-	     From :: gen_statem:from(),
+	     From :: from(),
 	     Message :: term(),
 	     State :: agent_state(),
 	     Data0 :: term(),
@@ -144,63 +149,142 @@
 		event_timer :: undefined | reference()
 	}).
 
+-spec start(Module, Args, Opts) -> Result
+	when Module :: module(),
+	     Args :: term(),
+	     Opts :: [start_opt()],
+	     Result :: start_ret().
 start(Module, Args, Opts) ->
 	gen_statem:start(?MODULE, {Module, Args}, Opts).
 
+-spec start(ServerName, Module, Args, Opts) -> Result
+	when ServerName :: server_ref(),
+	     Module :: module(),
+	     Args :: term(),
+	     Opts :: [start_opt()],
+	     Result :: start_ret().
 start(ServerName, Module, Args, Opts) ->
 	gen_statem:start(ServerName, ?MODULE, {Module, Args}, Opts).
 
+-spec start_link(Module, Args, Opts) -> Result
+	when Module :: module(),
+	     Args :: term(),
+	     Opts :: [start_opt()],
+	     Result :: start_ret().
 start_link(Module, Args, Opts) ->
 	gen_statem:start_link(?MODULE, {Module, Args}, Opts).
 
+-spec start_link(ServerName, Module, Args, Opts) -> Result
+	when ServerName :: server_ref(),
+	     Module :: module(),
+	     Args :: term(),
+	     Opts :: [start_opt()],
+	     Result :: start_ret().
 start_link(ServerName, Module, Args, Opts) ->
 	gen_statem:start_link(ServerName, ?MODULE, {Module, Args}, Opts).
 
+-spec start_monitor(Module, Args, Opts) -> Result
+	when Module :: module(),
+	     Args :: term(),
+	     Opts :: [start_opt()],
+	     Result :: start_mon_ret().
 start_monitor(Module, Args, Opts) ->
 	gen_statem:start_monitor(?MODULE, {Module, Args}, Opts).
 
+-spec start_monitor(ServerName, Module, Args, Opts) -> Result
+	when ServerName :: server_ref(),
+	     Module :: module(),
+	     Args :: term(),
+	     Opts :: [start_opt()],
+	     Result :: start_mon_ret().
 start_monitor(ServerName, Module, Args, Opts) ->
 	gen_statem:start_monitor(ServerName, ?MODULE, {Module, Args}, Opts).
 
+-spec perform(ServerRef) -> Result
+	when ServerRef :: server_ref(),
+	     Result :: ok | {error, Reason},
+	     Reason :: term().
 perform(ServerRef) ->
 	call(ServerRef, ?TAG_I(perform)).
 
+-spec wait(ServerRef) -> ok
+	when ServerRef :: server_ref().
 wait(ServerRef) ->
 	wait(ServerRef, infinity).
 
+-spec wait(ServerRef, Timeout) -> ok
+	when ServerRef :: server_ref(),
+	     Timeout :: timeout().
 wait(ServerRef, Timeout) ->
 	call(ServerRef, ?TAG_I(wait), Timeout).
 
+-spec call_cmd(ServerRef, Message) -> Reply
+	when ServerRef :: server_ref(),
+	     Message :: term(),
+	     Reply :: term().
 call_cmd(ServerRef, Msg) ->
 	call(ServerRef, ?TAG_C(Msg)).
 
+-spec call_cmd(ServerRef, Message, Timeout) -> Reply
+	when ServerRef :: server_ref(),
+	     Message :: term(),
+	     Timeout :: timeout(),
+	     Reply :: term().
 call_cmd(ServerRef, Msg, Timeout) ->
 	call(ServerRef, ?TAG_C(Msg), Timeout).
 
+-spec cast_cmd(ServerRef, Message) -> ok
+	when ServerRef :: server_ref(),
+	     Message :: term().
 cast_cmd(ServerRef, Msg) ->
 	cast(ServerRef, ?TAG_C(Msg)).
 
+-spec call(ServerRef, Message) -> Reply
+	when ServerRef :: server_ref(),
+	     Message :: term(),
+	     Reply :: term().
 call(ServerRef, Msg) ->
 	gen_statem:call(ServerRef, Msg).
 
+-spec call(ServerRef, Message, Timeout) -> Reply
+	when ServerRef :: server_ref(),
+	     Message :: term(),
+	     Timeout :: timeout(),
+	     Reply :: term().
 call(ServerRef, Msg, Timeout) ->
 	gen_statem:call(ServerRef, Msg, Timeout).
 
+-spec cast(ServerRef, Message) -> ok
+	when ServerRef :: server_ref(),
+	     Message :: term().
 cast(ServerRef, Msg) ->
 	gen_statem:cast(ServerRef, Msg).
 
+-spec reply(From, Message) -> ok
+	when From :: from(),
+	     Message :: term().
 reply(From, Reply) ->
 	gen_statem:reply(From, Reply).
 
+-spec stop(ServerRef) -> ok
+	when ServerRef :: server_ref().
 stop(ServerRef) ->
 	gen_statem:stop(ServerRef).
 
+-spec stop(ServerRef, Reason, Timeout) -> ok
+	when ServerRef :: server_ref(),
+	     Reason :: term(),
+	     Timeout :: timeout().
 stop(ServerRef, Reason, Timeout) ->
 	gen_statem:stop(ServerRef, Reason, Timeout).
 
+-spec callback_mode() -> handle_event_function.
 callback_mode() ->
 	handle_event_function.
 
+-spec init({CbMod, CbArgs}) -> gen_statem:init_result(agent_state())
+	when CbMod :: module(),
+	     CbArgs :: term().
 init({CbMod, CbArgs}) ->
 	case CbMod:init(CbArgs) of
 		{ok, CbState} ->
@@ -211,6 +295,11 @@ init({CbMod, CbArgs}) ->
 			Stop
 	end.
 
+-spec handle_event(EventType, Message, State, Data) -> gen_statem:event_handler_result(agent_state())
+	when EventType :: gen_statem:event_type(),
+	     Message :: ?TAG_I(term()) | ?TAG_C(term()) | term(),
+	     State :: agent_state(),
+	     Data :: #data{}.
 handle_event({call, From}, ?TAG_I(perform), idle, Data) ->
 	{next_state, sleeping, Data#data{attempt=0}, [{next_event, internal, ?TAG_I(enter)}, {reply, From, ok}]};
 handle_event({call, From}, ?TAG_I(perform), State, _Data) ->
@@ -304,6 +393,11 @@ cancel_timer(Timer) ->
 			ok
 	end.
 
+-spec terminate(Reason, State, Data) -> Ignored
+	when Reason :: term(),
+	     State :: agent_state(),
+	     Data :: #data{},
+	     Ignored :: term().
 terminate(Reason, State, #data{cb_mod=CbMod, cb_state=CbState}) ->
 	case erlang:function_exported(CbMod, terminate, 3) of
 		true ->
@@ -312,6 +406,15 @@ terminate(Reason, State, #data{cb_mod=CbMod, cb_state=CbState}) ->
 			ok
 	end.
 
+-spec code_change(OldVsn, State0, Data0, Extra) -> Result
+	when OldVsn :: term() | {down, term()},
+	     State0 :: agent_state(),
+	     Data0 :: term(),
+	     Extra :: term(),
+	     Result :: {ok, State1, Data1} | Reason,
+	     State1 :: agent_state(),
+	     Data1 :: term(),
+	     Reason :: term().
 code_change(OldVsn, State, Data=#data{cb_mod=CbMod, cb_state=CbState0}, Extra) ->
 	case erlang:function_exported(CbMod, code_change, 4) of
 		true ->
